d841a2f9a92da81f64e825995d4eddf1420c011b
haoknowah <nrgman@tds.net>
2023-10-13 19:14:27 -0500

    minor

--- src/main/java/wilson/functions/ModData.java
+++ src/main/java/wilson/functions/ModData.java
@@ -1,19 +1,31 @@
 package wilson.functions;
 
 import java.io.FileReader;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
 
-import org.json.JSONObject;
+
 import org.json.simple.parser.JSONParser;
+
+import com.google.gson.Gson;
+
+import wilson.models.Category;
 
 public class ModData {
 	public static void addToCategory(String culprit, String CategoryName)
 	{
 		JSONParser par = new JSONParser();
+		Gson gson = new Gson();
 		try
 		{
 			Object obj = par.parse(new FileReader(System.getProperty("user.dir") + "/categories.json"));
-			JSONObject json = (JSONObject) obj;
-			if(json.has(CategoryName))
+			System.out.println(obj.toString());
+			System.out.println(par.parse(new FileReader(System.getProperty("user.dir") + "/categories.json")));
+			Reader yub = new FileReader(System.getProperty("user.dir") + "/categories.json");
+			List<Category> cats = new ArrayList<Category>();
+			Category r = gson.fromJson(yub, Category.class);
+			if(r.getCulprits().contains(CategoryName))
 			{
 				System.out.println("MEOW");
 			}
